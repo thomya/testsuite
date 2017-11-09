c7feec8072683ac99608dfa55aae369c5fb076f2
yangzp <yangzp@tcl.com>
2017-11-09 17:58:05 +0800

    build prepare .

---
+++ tools/buildprepare.sh
@@ -0,0 +1,32 @@
+  source build/envsetup.sh;lunch
+  mmm cts/tools/cts-java-scanner-doclet/
+  mmm cts/tools/cts-java-scanner
+  mmm cts/libs/json/
+  mmm cts/libs/vogar-expect/
+  mmm cts/tools/cts-xml-generator/
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
