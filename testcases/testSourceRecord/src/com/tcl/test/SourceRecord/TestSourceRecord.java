63fe70819e7d1c9c6c86ebb00a2f716527c8d4ce
yangzp <yangzp@tcl.com>
2017-11-17 11:20:16 +0800

    test for source record common mechanism

--- testcases/testSourceRecord/src/com/tcl/test/SourceRecord/TestSourceRecord.java
+++ testcases/testSourceRecord/src/com/tcl/test/SourceRecord/TestSourceRecord.java
@@ -16,25 +16,15 @@ import android.os.SystemClock;
 
 public class TestSourceRecord extends AndroidTestCase{
     private static final String TAG = "TestSourceRecord";
+    private static final String BOOTFROMUI = "persist.sys.bootfromui";
+    private static final String BOOTPACKAGE = "persist.sys.bootpackage";
     private ActivityManager mAm;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mAm = (ActivityManager)(getContext().getSystemService(Context.ACTIVITY_SERVICE));
-        long now = SystemClock.uptimeMillis();
-        KeyEvent down = new KeyEvent(now, now, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HOME, 0,0,-1,65,0,0);
-        KeyEvent up = new KeyEvent(now, now, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HOME, 0,0,-1,65,0,0);
-        try{
-            Class cls = InputManager.class;
-            Method instance = cls.getMethod("getInstance",null);
-            Object inputManager = instance.invoke(null);
-            Method injectEvent = cls.getMethod("injectInputEvent",InputEvent.class,int.class);
-            injectEvent.invoke(inputManager,down,2);
-            injectEvent.invoke(inputManager,up,2);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        injectKey(KeyEvent.KEYCODE_HOME);
     }
 
     public void testAmNotNull(){
@@ -42,40 +32,124 @@ public class TestSourceRecord extends AndroidTestCase{
     }
 
     public void testSourceRecord(){
-        assertTrue(getRecordSource().equals(getTopActivity()));
-        long now = SystemClock.uptimeMillis();
-        KeyEvent down = new KeyEvent(now, now, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HOME, 0,0,-1,65,0,0);
-        KeyEvent up = new KeyEvent(now, now, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HOME, 0,0,-1,65,0,0);
-        try{
-            Class cls = InputManager.class;
-            Method instance = cls.getMethod("getInstance",null);
-            Object inputManager = instance.invoke(null);
-            Method injectEvent = cls.getMethod("injectInputEvent",InputEvent.class,int.class);
-            injectEvent.invoke(inputManager,down,2);
-            injectEvent.invoke(inputManager,up,2);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        String bootfromui = "";
+        String bootTo = "";
+        String TopActivity = "";
+
+        bootfromui = getProperty(BOOTFROMUI);
+        TopActivity = getTopActivity();
+        assertTrue(!"".equals(TopActivity));
+        bootTo = TopActivity.equals("com.tcl.tv") ? "tv" : "ui";
+
+        assertTrue(!"".equals(bootfromui));
+        assertTrue(!"".equals(bootTo));
+        assertTrue(bootfromui.equals(bootTo));
+        
+        injectKey(KeyEvent.KEYCODE_HOME);
+        
         try{
             Thread.sleep(500);
         }catch(Exception e){
             e.printStackTrace();
         }
 
-        assertTrue(getRecordSource().equals(getTopActivity()));
+        bootfromui = getProperty(BOOTFROMUI);
+        TopActivity = getTopActivity();
+        assertTrue(!"".equals(TopActivity));
+        bootTo = TopActivity.equals("com.tcl.tv") ? "tv" : "ui";
+
+        assertTrue(!"".equals(bootfromui));
+        assertTrue(!"".equals(bootTo));
+        assertTrue(bootfromui.equals(bootTo));
+    
     }
 
-    private String getRecordSource(){
+    public void testCommonSourceRecord(){
+        String bootfromui = "";
+        String bootPackage = "";
+        String bootTo = "";
+        String TopActivity = "";
+
+        bootfromui = getProperty(BOOTFROMUI);
+        bootPackage = getProperty(BOOTPACKAGE);
+        TopActivity = getTopActivity();
+        assertTrue(!"".equals(TopActivity));
+        bootTo = TopActivity.equals("com.tcl.tv") ? "tv" : "ui";
+
+        assertTrue(!"".equals(bootfromui));
+        assertTrue(!"".equals(bootPackage));
+        assertTrue(!"".equals(bootTo));
+        assertTrue(bootfromui.equals(bootTo));
+        assertTrue(bootPackage.equals(TopActivity));
+        
+        injectKey(KeyEvent.KEYCODE_HOME);
+        
+        try{
+            Thread.sleep(500);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        bootfromui = getProperty(BOOTFROMUI);
+        bootPackage = getProperty(BOOTPACKAGE);
+        TopActivity = getTopActivity();
+        assertTrue(!"".equals(TopActivity));
+        bootTo = TopActivity.equals("com.tcl.tv") ? "tv" : "ui";
+
+        assertTrue(!"".equals(bootfromui));
+        assertTrue(!"".equals(bootPackage));
+        assertTrue(!"".equals(bootTo));
+        assertTrue(bootfromui.equals(bootTo));
+        assertTrue(bootPackage.equals(TopActivity));
+    }
+
+    private String getProperty(String key){
         String value = "";
         try{
             Class cls = Class.forName("android.os.SystemProperties");
             Method getprop = cls.getMethod("get",String.class,String.class);
-            value = (String)getprop.invoke(null,"persist.sys.bootfromui","");
+            value = (String)getprop.invoke(null,key,"");
         }catch(Exception e ){
             e.printStackTrace();
         }
-        Log.d(TAG,"getRecordSource() : " + value);
+        Log.d(TAG,"getproperty : [ " + key + " , " + value + " ];");
         return value;
+    }
+
+    private void injectKey(int keyCode){
+        long now = SystemClock.uptimeMillis();
+        KeyEvent down = new KeyEvent(now, now, KeyEvent.ACTION_DOWN,keyCode , 0,0,-1,65,0,0);
+        Class cls;
+        Method instance;
+        Object inputManager;
+        Method injectEvent;
+        try{
+            cls = InputManager.class;
+            instance = cls.getMethod("getInstance",null);
+            inputManager = instance.invoke(null);
+            injectEvent = cls.getMethod("injectInputEvent",InputEvent.class,int.class);
+            injectEvent.invoke(inputManager,down,2);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            Thread.sleep(100);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        now = SystemClock.uptimeMillis();
+        KeyEvent up = new KeyEvent(now, now, KeyEvent.ACTION_UP, keyCode, 0,0,-1,65,0,0);
+        try{
+            cls = InputManager.class;
+            instance = cls.getMethod("getInstance",null);
+            inputManager = instance.invoke(null);
+            injectEvent = cls.getMethod("injectInputEvent",InputEvent.class,int.class);
+            injectEvent.invoke(inputManager,up,2);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     private String getTopActivity(){
@@ -87,7 +161,7 @@ public class TestSourceRecord extends AndroidTestCase{
         }else{
             Log.d(TAG,"getTopActivity failed .");
         }
-        return packageName.equals("com.tcl.tv") ? "tv" : "ui";
+        return packageName;
     }
 
     @Override
